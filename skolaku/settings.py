5b93372b20884c1b12fbb8e6732d3a8fe49fc3ad
kneeraazon01 <kneeraazon@gmail.com>
2022-05-07 03:15:14 +0545

    first version ready to release

--- skolaku/settings.py
+++ skolaku/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'qzvrsy-b%-dmpi!m$-tsajlh2ifu_&y-=c7_$9-%a^8afhw!!!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 DEFAULT_AUTO_FIELD="django.db.models.AutoField"
